90b2b2eb0ed17279706a49c3501175e2bd998e51
pedram <pedram.pourmajidian@gmail.cpm>
2023-05-14 15:21:23 +0330

    add tag to user

--- app/app/urls.py
+++ app/app/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
+    path('app/recipe/', include('recipe.urls')),
 ]
